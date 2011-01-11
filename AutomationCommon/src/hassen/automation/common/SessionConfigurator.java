681cd0d30503031339c5b044f38cccaccfa72c4d
David Brun <brundavid@gmail.com>
2011-01-12 00:21:32 +0100

    Implémentation de la méthode clearMaps
    Signed-off-by: David Brun <brundavid@gmail.com>

--- AutomationCommon/src/hassen/automation/common/SessionConfigurator.java
+++ AutomationCommon/src/hassen/automation/common/SessionConfigurator.java
@@ -86,7 +86,24 @@ public class SessionConfigurator {
 	}
 
 	public boolean clearMaps () {
-		return false;
+		try
+		{
+			connect();
+			ManagementWriter w = new ManagementWriter(connection.getOutputStream());
+			w.createQueryMapClear();
+			w.send();
+			ManagementReader r = new ManagementReader(connection.getInputStream());
+			r.receive();
+			if (r.getType() == Protocol.REPLY_SUCCESS)
+				return true;
+			else
+				return false;
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	public boolean eraseMap (int id)
